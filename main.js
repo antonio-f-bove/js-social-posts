51b72804d6159826e1e4defd8ec53d8891260d10
AntonioFBove <antoniofbove@gmail.com>
2022-01-11 16:41:04 +0100

    html injection not working

--- main.js
+++ main.js
@@ -1,26 +1,77 @@
-const postList = [
+function printFeed(posts, container) {
+  // reset dell'eventuale contenuto
+  container.innerHTML = '';
+
+  for (let i = 0; i < posts.lenght; i++) {
+    const { id, author, profilePic, date, contentText, contentPic } = posts[i];
+
+    container.innerHTML += `
+    <div id="${id}" class="post">
+      <div class="post__header">
+          <div class="post-meta">                    
+              <div class="post-meta__icon">
+                  <img class="profile-pic" src="${profilePic}" alt="${author}">                    
+              </div>
+              <div class="post-meta__data">
+                  <div class="post-meta__author">${author}</div>
+                  <div class="post-meta__time">${date}</div>
+              </div>                    
+          </div>
+      </div>
+      <div class="post__text">${contentText}</div>
+      <div class="post__image">
+          <img src="${contentPic}" alt="">
+      </div>
+      <div class="post__footer">
+          <div class="likes js-likes">
+              <div class="likes__cta">
+                  <a class="like-button  js-like-button" href="#" data-postid="1">
+                      <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                      <span class="like-button__label">Mi Piace</span>
+                  </a>
+              </div>
+              <div class="likes__counter">
+                  Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+              </div>
+          </div> 
+      </div>            
+    </div>
+  `;
+  }
+
+  
+
+};
+
+
+const postsList = [
   {
     id: 1,
     author: 'Phil Mangione',
-    profilePic: 'https://unsplash.it/300/300?image=15',
+    profilePic: 'https://unsplash.it/300/300?image=',
     date: '10/21/2021',
     contentText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-    contentPic: 'https://unsplash.it/600/300?image=171',
+    contentPic: 'https://unsplash.it/600/300?image=',
   },
   {
     id: 1,
     author: 'Johnny Stecchino',
-    profilePic: 'https://unsplash.it/300/300?image=15',
+    profilePic: 'https://unsplash.it/300/300?image=',
     date: '10/27/2021',
     contentText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-    contentPic: 'https://unsplash.it/600/300?image=171',
+    contentPic: 'https://unsplash.it/600/300?image=',
   },
   {
     id: 1,
     author: 'Arnold Neronero',
-    profilePic: 'https://unsplash.it/300/300?image=15',
+    profilePic: 'https://unsplash.it/300/300?image=',
     date: '11/30/2021',
     contentText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-    contentPic: 'https://unsplash.it/600/300?image=171',
+    contentPic: 'https://unsplash.it/600/300?image=',
   },
 ];
+
+
+const postContainerHtml = document.getElementById('container');
+
+printFeed(postsList, postContainerHtml);
