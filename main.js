acad51ef2311516b38bbc65bc141528ca4929f45
AntonioFBove <antoniofbove@gmail.com>
2022-01-11 16:01:52 +0100

    milestone 1: array di 3 oggetti

---
+++ main.js
@@ -0,0 +1,26 @@
+const postList = [
+  {
+    id: 1,
+    author: 'Phil Mangione',
+    profilePic: 'https://unsplash.it/300/300?image=15',
+    date: '10/21/2021',
+    contentText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+    contentPic: 'https://unsplash.it/600/300?image=171',
+  },
+  {
+    id: 1,
+    author: 'Johnny Stecchino',
+    profilePic: 'https://unsplash.it/300/300?image=15',
+    date: '10/27/2021',
+    contentText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+    contentPic: 'https://unsplash.it/600/300?image=171',
+  },
+  {
+    id: 1,
+    author: 'Arnold Neronero',
+    profilePic: 'https://unsplash.it/300/300?image=15',
+    date: '11/30/2021',
+    contentText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+    contentPic: 'https://unsplash.it/600/300?image=171',
+  },
+];
